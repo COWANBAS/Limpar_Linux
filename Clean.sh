f71ba7c1bcb59c13df7409605e0c347c3180ea63
Cowanbas <131304556+COWANBAS@users.noreply.github.com>
2026-01-19 22:55:52 -0300

    Update Clean.sh

--- Clean.sh
+++ Clean.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# Limpar cache de pacotes
-echo "Limpando cache de pacotes..."
-sudo apt-get clean
-sudo apt-get autoclean
-
 # Remover pacotes não utilizados
 echo "Removendo pacotes não utilizados..."
 sudo apt-get autoremove --purge -y
 sudo apt-get autoremove
 sudo apt-get remove
 
-# Limpar conexões de pacotes ausentes
+# Limpar cache de pacotes
+echo "Limpando cache de pacotes..."
+sudo apt-get autoclean
+sudo apt-get clean
+
+# Limpar pacotes ausentes
 echo "Removendo pacotes ausentes..."
 sudo apt-get autoremove --purge -y
 sudo apt-get autoclean
 sudo apt-get clean
 
-# Limpar logs antigos
-echo "Limpando logs antigos..."
-sudo journalctl --vacuum-time=1d 
+# Limpar logs do sistema
+echo "Limpando logs do sistema..."
+sudo journalctl --rotate && sudo journalctl --vacuum-time=1s
+sudo rm -rf /var/log/*.log*
 
 # Limpando comandos do terminal
 echo "Limpando comandos do terminal..."
@@ -27,9 +28,9 @@ sudo rm -rf ~/.bash_history*
 
 # Limpar logs de pacotes
 echo "Limpando logs do pacote..."
-sudo rm -f /var/log/apt/*.gz
 sudo rm -f /var/log/apt/history.log.*
 sudo rm -f /var/log/apt/term.log.*
+sudo rm -f /var/log/apt/*.gz
 
 # Limpar relatório de erros
 echo "Limpando relatório de erros..."
@@ -37,21 +38,22 @@ sudo rm -f ~/.xsession-errors*
 
 # Limpar arquivos temporários
 echo "Limpando arquivos temporários..."
-sudo rm -rf /tmp/*
+sudo rm -rf /var/cache/apt/*
 sudo rm -rf /var/tmp/*
+sudo rm -rf /tmp/*
 
 # Limpar cache de navegadores 
 echo "Limpando cache de navegadores..."
-rm -rf ~/.cache/google-chrome/*
-rm -rf ~/.config/Min/Cache*
-rm -rf ~/.cache/mozilla/*
-rm -rf ~/.cache/min/*
+sudo rm -rf ~/.cache/google-chrome/*
+sudo rm -rf ~/.config/Min/Cache*
+sudo rm -rf ~/.cache/mozilla/*
+sudo rm -rf ~/.cache/min/*
 
 # Limpar arquivos de "thumbnails"
 echo "Limpando cache de thumbnails..."
-rm -rf ~/.cache/thumbnails/*
-rm -rf ~/.cache/thumbnails/fail/*
-rm -rf ~/.cache/thumbnails/large/*
+sudo rm -rf ~/.cache/thumbnails/large/*
+sudo rm -rf ~/.cache/thumbnails/fail/*
+sudo rm -rf ~/.cache/thumbnails/*
 
 # Limpar arquivos de "spool" e "crash reports"
 echo "Limpando arquivos de spool e crash reports..."
@@ -60,28 +62,24 @@ sudo rm -rf /var/crash/*
 
 # Limpar arquivos temporários do wine
 echo "Limpando arquivos temporário do wine..."
-rm -rf ~/.wine/dosdevices/c:/windows/temp*
-rm -rf ~/.wine/drive_c/users/*/AppData/Local/Temp*
-rm -rf ~/.wine/drive_c/users/*/AppData/Roaming/Microsoft/Windows/Recent/*
-
-# Limpar pacotes orfãos, se houver
-echo "Removendo pacotes orfãos..."
-sudo apt-get autoremove --purge -y
+sudo rm -rf ~/.wine/drive_c/users/*/AppData/Roaming/Microsoft/Windows/Recent/*
+sudo rm -rf ~/.wine/drive_c/users/*/AppData/Local/Temp*
+sudo rm -rf ~/.wine/dosdevices/c:/windows/temp*
 
 # Limpar arquivos de configuração e pacotes não utilizados
 echo "Removendo pacotes e configurações não utilizados..."
-sudo rm -rf /var/lib/apt/lists/* /var/cache/apt/archives/*
-
-# Limpar logs do sistema
-echo "Limpando logs do sistema..."
-sudo rm -rf /var/log/*.log*
+sudo rm -rf /var/lib/apt/lists/*
 
 # Limpar arquivos temporários e caches de fontes
 echo "Limpando caches de fontes e arquivos temporários..."
-rm -rf ~/.cache/fontconfig/*
+sudo rm -rf ~/.cache/fontconfig/*
 
 # Limpar caches do Dns
 sudo systemd-resolve --flush-caches
+
+# Limpar cache do pip
+echo "Limpando caches do pip..."
+sudo pip3 cache purge
 
 clear
 
